e5d6a5d0f6d034cb857e6b859cb1e5da2dff131f
Jack Kingham <wowknead@gmail.com>
2025-08-18 21:59:57 -0600

    Added error handling to curvey redirect.

--- instance/instance-api.py
+++ instance/instance-api.py
@@ -242,12 +242,15 @@ def forward_to_survey():
     '''
     User has finished, now redirect to the exit survey.
     '''
-    with open(CONFIG.USER_DATA_FILE) as data_file:
-        user_data = json.load(data_file)
-        user_id = user_data["user_id"]
-        token = user_data["token"]
-        save_firefox_history(user_id, token)
-        return redirect("/survey/"+user_id+"/"+token)
+    try:
+        with open(CONFIG.USER_DATA_FILE) as data_file:
+            user_data = json.load(data_file)
+            user_id = user_data["user_id"]
+            token = user_data["token"]
+            save_firefox_history(user_id, token)
+            return redirect("/survey/"+user_id+"/"+token)
+    except Exception:
+        return "Unexpected Error 531"
 
 
 @app.route("/api/uptime", methods=['GET'])
